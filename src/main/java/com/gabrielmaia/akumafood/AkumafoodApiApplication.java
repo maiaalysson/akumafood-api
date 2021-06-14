d686c7e174d1611a5ada613fe889d2fc8f1a85c2
gabrielmaia <gabrielmaia@gabrielmaia-pc>
2021-06-14 01:06:01 -0300

    #000000 - Inserting project
    Creating a project for the spring rest EST specialist course from
    algaworks company

---
+++ src/main/java/com/gabrielmaia/akumafood/AkumafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielmaia.akumafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkumafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkumafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielmaia/akumafood/AkumafoodApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.gabrielmaia.akumafood;
+
+//@SpringBootTest
+class AkumafoodApiApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
